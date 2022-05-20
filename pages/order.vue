0f1959eee9940f7e37f53a1d1e466e8b3975f4a2
Wanda Alifah <wanda.alifah@students.unnes.ac.id>
2022-05-20 18:32:34 +0700

    update code

--- pages/order.vue
+++ pages/order.vue
@@ -4,7 +4,7 @@
             <h3 class="mt-8">Insert Your Reservation Code</h3>
             <v-layout>
                 <v-flex>
-                    <v-text-field label="Booking code" :v-model="order_code"></v-text-field>
+                    <v-text-field v-model="insertedCode" label="Booking code"></v-text-field>
                 </v-flex>
                 <v-flex>
                     <v-btn
@@ -12,27 +12,30 @@
                         :loading="loading"
                         :disabled="loading"
                         color="secondary"
-                        @click="loader = 'loading', showDetail()"
+                        @click="loader = 'loading', showDetail(insertedCode)"
                     >Submit</v-btn>
                 </v-flex>
             </v-layout>
         </v-container>
         <v-container>
-            <h3 class="mb-4">Reservation Details</h3>
-            <v-card v-for="order in cafeCoffee_orders" :key="order.id">
-                <div v-for="user in order.detail_orders" :key="user">
-                    <v-card-text>Name : {{ user.username }}</v-card-text>
-                    <v-card-text>Phone Number : {{ user.phone_number }} </v-card-text>
-                </div>
-                <hr>
-                <v-card-text>Reservation date : {{ order.reserv_date }} {{ order.reserv_time }}</v-card-text>
-                <v-card-text>Menu ordered :
-                    <ul v-for="list in order.ordered_menus" :key="list">
-                        <li>{{ list }}</li>
-                    </ul>
-                </v-card-text>
-                <v-card-text>Total : {{ order.total_order }}</v-card-text>
-            </v-card>
+            <div v-for="order in orders" :key="order.id">
+                <h3 class="mb-4">Reservation Details</h3>
+                <v-card>
+                    <div v-for="user in order.detail_orders" :key="user">
+                        <v-card-text>Name : {{ user.username }}</v-card-text>
+                        <v-card-text>Phone Number : {{ user.phone_number }} </v-card-text>
+                    </div>
+                    <hr>
+                    <v-card-text>Order Code : {{ order.order_code }}</v-card-text>
+                    <v-card-text>Reservation date : {{ order.reserv_date }} {{ order.reserv_time }}</v-card-text>
+                    <v-card-text>Menu ordered :
+                        <ul v-for="list in order.ordered_menus" :key="list">
+                            <li>{{ list }}</li>
+                        </ul>
+                    </v-card-text>
+                    <v-card-text>Total : {{ order.total_order }}</v-card-text>
+                </v-card>
+            </div>
         </v-container>
     </v-main>
 </template>
@@ -45,14 +48,14 @@ export default {
         return {
             loader: null,
             loading: false,
-            order_code: "5d85911e-192c-4565-b4c6-def24626485d",
-            // orders: [this.$apollo.orders]
+            insertedCode: "",
+            orders: []
         }
     },
     apollo: {
         cafeCoffee_orders: {
-                query: gql `query CafeOrder($order_code: uuid){
-                    cafeCoffee_orders(where: {order_code: {_eq: $order_code}}){
+            query: gql `query CafeOrder{
+                cafeCoffee_orders{
                     id
                     order_code
                     ordered_menus
@@ -62,14 +65,9 @@ export default {
                     detail_orders {
                         phone_number
                         username
-                        }
                     }
-                }`,
-                variables() {
-                    return {
-                        order_code: this.order_code
-                    }
-                },
+                }
+            }`,            
         }
     },
     watch: {
@@ -82,5 +80,41 @@ export default {
         this.loader = null
       },
     },
+    methods: {
+        showDetail(insertedCode) {
+            this.$apollo.query({
+                query: gql`query CodeAdded($order_code: uuid) {
+                    cafeCoffee_orders(where: { order_code: { _eq: $order_code } }) {
+                        id
+                        order_code
+                        ordered_menus
+                        reserv_date
+                        reserv_time
+                        total_order
+                        detail_orders {
+                            phone_number
+                            username
+                        }
+                    }
+                }`,
+                variables() {
+                    return {
+                        order_code: insertedCode,
+                    }
+                },
+                updateQueries: {
+                    cafeCoffeeQuery: (prev, { queryResult }) => {
+                        const newOrder = queryResult.data.cafeCoffee_orders
+                        const newOrders = [newOrder, ...prev.cafeCoffee_orders]
+                        return {
+                            cafeCoffee_orders: newOrders,
+                        }
+                    },
+                },
+            })
+            const currentOrder = this.$apollo.cafeCoffee_orders
+            this.orders.push({...currentOrder})
+        }
+    }
 }
 </script>
